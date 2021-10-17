049ec0e74d0ab589513d4d3f968db283fd8c2061
Nicole Abadie <nic4096@gmail.com>
2021-10-17 05:55:15 -0700

    Removing 'about me' section until it's finished. Prettier spacing in buildPortfolio.go

--- buildPortfolio/buildPortfolio.go
+++ buildPortfolio/buildPortfolio.go
@@ -1,152 +1,152 @@
 package main
 
 import (
-	"io/ioutil"
-	"text/template"
-	"encoding/csv"
-	"bytes"
+    "io/ioutil"
+    "text/template"
+    "encoding/csv"
+    "bytes"
     "flag"
-	"os"
-	"fmt"
-	"strings"
-	"time"
-	"github.com/fatih/color"
+    "os"
+    "fmt"
+    "strings"
+    "time"
+    "github.com/fatih/color"
 )
 
 func main() {
-	// Starting execution timer
-	start := time.Now()
+    // Starting execution timer
+    start := time.Now()
 
-	// Flags
-	csvFileDir := flag.String("csv", "data.csv",
-		"A CSV file containing data to be inserted into the template.")
-	tmplDir := flag.String("tmpl", "template.tmpl",
-		"A .tmpl file to insert data into.")
-	outputDir := flag.String("output", "../index.html",
-		"The .html output file.")
-	flag.Parse()
+    // Flags
+    csvFileDir := flag.String("csv", "data.csv",
+        "A CSV file containing data to be inserted into the template.")
+    tmplDir := flag.String("tmpl", "template.tmpl",
+        "A .tmpl file to insert data into.")
+    outputDir := flag.String("output", "../index.html",
+        "The .html output file.")
+    flag.Parse()
 
-	// Correcting any missing extensions for the given flags
-	if strings.LastIndex(*csvFileDir, ".") < 0 {
-		*csvFileDir += ".csv"
-	}
-	if strings.LastIndex(*tmplDir, ".") < 0 {
-		*tmplDir += ".tmpl"
-	}
-	if strings.LastIndex(*outputDir, ".") < 0 {
-		*outputDir += ".html"
-	}
+    // Correcting any missing extensions for the given flags
+    if strings.LastIndex(*csvFileDir, ".") < 0 {
+        *csvFileDir += ".csv"
+    }
+    if strings.LastIndex(*tmplDir, ".") < 0 {
+        *tmplDir += ".tmpl"
+    }
+    if strings.LastIndex(*outputDir, ".") < 0 {
+        *outputDir += ".html"
+    }
 
-	// Do all the work & get filesize
-	var fileSize float64
-	data := getData(*csvFileDir)
-	fileSize += createAndSaveFile(*tmplDir, *outputDir, data)
+    // Do all the work & get filesize
+    var fileSize float64
+    data := getData(*csvFileDir)
+    fileSize += createAndSaveFile(*tmplDir, *outputDir, data)
 
-	// Pretty font styling
-	boldGreen := color.New(color.FgGreen, color.Bold).SprintFunc()
-	bold := color.New(color.Bold).SprintFunc()
+    // Pretty font styling
+    boldGreen := color.New(color.FgGreen, color.Bold).SprintFunc()
+    bold := color.New(color.Bold).SprintFunc()
 
-	// Get execution time in seconds
-	exeTime := time.Since(start).Seconds()
+    // Get execution time in seconds
+    exeTime := time.Since(start).Seconds()
 
-	// Success message :)
-	fmt.Printf("%s Generated %s (%.1fkB total) in %.3f seconds.\n",
-		boldGreen("[Success!]"), bold(outputDir), fileSize, exeTime)
+    // Success message :)
+    fmt.Printf("%s Generated %s (%.1fkB total) in %.3f seconds.\n",
+    boldGreen("[Success!]"), bold(outputDir), fileSize, exeTime)
 }
 
 // Struct of everything to be inserted into template
 type portfolioData struct {
     Projects []itemData
-	Articles []itemData
-	Footer []itemData
+    Articles []itemData
+    Footer []itemData
 }
 
 // This is how each item is structured
 type itemData struct {
     Url string
-	Title string // For a footer item, this is the fontawesome icon classes
-	Desc string // For a footer item, this is the text
+    Title string // For a footer item, this is the fontawesome icon classes
+    Desc string // For a footer item, this is the text
 }
 
 
 func readCsvFile(csvFile string) [][]string {
-	// Open the csv file, throw err if something goes wrong
+    // Open the csv file, throw err if something goes wrong
     f, err := os.Open(csvFile)
     checkErr(err, fmt.Sprintf("while opening %s!\n", csvFile))
     defer f.Close()
 
-	// Read the csv file, throw err if something goes wrong
+    // Read the csv file, throw err if something goes wrong
     csvReader := csv.NewReader(f)
     csvData, err := csvReader.ReadAll()
     checkErr(err, fmt.Sprintf("while reading %s!\n", csvFile))
 
-	// Return 2d slice of the data from the given csv file
+    // Return 2d slice of the data from the given csv file
     return csvData
 }
 
 func getData(csvFileDir string) portfolioData {
-	// Get raw data from csv
-	data := readCsvFile(csvFileDir)
+    // Get raw data from csv
+    data := readCsvFile(csvFileDir)
 
-	// Setup vars for later
-	var projects []itemData
-	var articles []itemData
-	var footer []itemData
-	var item itemData
+    // Setup vars for later
+    var projects []itemData
+    var articles []itemData
+    var footer []itemData
+    var item itemData
 
-	// Sort thru the data from the csv
-	for _, line := range data {
-		// Skip iteration if header, empty line, or missing field
-		if line[0] == "TYPE" || line[0] == "" || len(line) < 4 {
-			continue
-		}
+    // Sort thru the data from the csv
+    for _, line := range data {
+        // Skip iteration if header, empty line, or missing field
+        if line[0] == "TYPE" || line[0] == "" || len(line) < 4 {
+            continue
+        }
 
-		// Build the item
-		item = itemData {
-			Title: line[1],
-			Desc: line[2],
-			Url: line[3],
-		}
-		// Append to appropriate slice
-		switch line[0] {
-			case "P": // Project
-				projects = append(projects, item)
-			case "A": // Article
-				articles = append(articles, item)
-			case "F": // Footer
-				footer = append(footer, item)
-		}
+        // Build the item
+        item = itemData {
+            Title: line[1],
+            Desc: line[2],
+            Url: line[3],
+        }
+        // Append to appropriate slice
+        switch line[0] {
+            case "P": // Project
+                projects = append(projects, item)
+            case "A": // Article
+                articles = append(articles, item)
+            case "F": // Footer
+                footer = append(footer, item)
+        }
     }
-	// Return struct of all the data packaged together for the template
-	return portfolioData{Projects: projects, Articles: articles, Footer: footer}
+    // Return struct of all the data packaged together for the template
+    return portfolioData{Projects: projects, Articles: articles, Footer: footer}
 }
 
 func createAndSaveFile(tmplDir string, outputDir string, data portfolioData) float64 {
-	// Parse template
-	tmpl := template.Must(template.New(tmplDir).ParseFiles(tmplDir))
+    // Parse template
+    tmpl := template.Must(template.New(tmplDir).ParseFiles(tmplDir))
 
-	//  Insert data into template & save bytes to buffer, throw err if something goes wrong
-	buffer := new(bytes.Buffer)
-	err := tmpl.Execute(buffer, data)
-	checkErr(err, fmt.Sprintf("while parsing template %s!\n", tmplDir))
+    //  Insert data into template & save bytes to buffer, throw err if something goes wrong
+    buffer := new(bytes.Buffer)
+    err := tmpl.Execute(buffer, data)
+    checkErr(err, fmt.Sprintf("while parsing template %s!\n", tmplDir))
 
-	// Convert write bytes into output file, throw err if something goes wrong
-	bytesToWrite := []byte(buffer.String())
-	err = ioutil.WriteFile(outputDir, bytesToWrite, 0644)
-	checkErr(err, fmt.Sprintf("while creating the output file %s!\n", outputDir))
+    // Convert write bytes into output file, throw err if something goes wrong
+    bytesToWrite := []byte(buffer.String())
+    err = ioutil.WriteFile(outputDir, bytesToWrite, 0644)
+    checkErr(err, fmt.Sprintf("while creating the output file %s!\n", outputDir))
 
-	// Return filesize (kB) of the new html file
-	return float64(len(bytesToWrite)) / float64(1000)
+    // Return filesize (kB) of the new html file
+    return float64(len(bytesToWrite)) / float64(1000)
 }
 
 func checkErr(err error, msg string) {
-	// Check for error
-	if err != nil {
-		// String formatting
-		boldRed := color.New(color.FgRed, color.Bold).SprintFunc()
-		// Print pretty error message
-		fmt.Println(boldRed("[Error]"), msg)
-		// Print the raw err msg below the pretty one for more info
-	    panic(err)
-	}
+    // Check for error
+    if err != nil {
+        // String formatting
+        boldRed := color.New(color.FgRed, color.Bold).SprintFunc()
+        // Print pretty error message
+        fmt.Println(boldRed("[Error]"), msg)
+        // Print the raw err msg below the pretty one for more info
+        panic(err)
+    }
 }
